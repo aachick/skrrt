006504abe5b1933aacd971866facf55f28afeffe
aachick <Chick@chicks-mbp-1.home>
2019-12-31 17:27:15 +0100

    Fixed typo

--- setup.py
+++ setup.py
@@ -6,17 +6,19 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='skrrt',
-    version='0.0.1',
+    version='1.0.0a1',
     author='aachick',
     description='skrrt is a package designed to play audio files during the execution of other routines',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/aachick/skrrt',
-    package_data=setuptools.find_packages(),
+    packages=setuptools.find_packages(),
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent'
     ],
-    python_requires='>=3.5'
+    python_requires='>=3'
 )
